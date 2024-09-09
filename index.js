b2092a26f3541b9dba8382d3f1a26d863edb0549
rockymoto377 <rockymoto377@gmail.com>
2024-09-08 22:34:05 -0500

    Initial URL shortener functionality

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const URL = require('url').URL;
+
+// Setup URL array
+let ShortURLs = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+// POST request body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cors());
 
@@ -17,6 +26,37 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// Helper to check if a URL is valid
+const checkURL = (u) => {
+	try {
+		new URL(u);
+		return true;
+	} catch (e) {
+		return false;
+	}
+};
+
+// URL Shortener API endpoint
+app.route('/api/shorturl').post(function(req, res) {
+	if (checkURL(req.body.url)) {
+		res.json({ original_url: req.body.url, short_url: ShortURLs.length });
+		ShortURLs.push(req.body.url);
+	} else {
+		res.json({ error: "invalid url" });
+	}
+});
+
+app.get('/api/shorturl/:index', function(req, res, next) {
+	let index = req.params.index;
+	if (index < 1) {
+		res.json({ error: "Wrong format" });
+	} else if (index > ShortURLs.length) {
+		res.json({ error: "No short URL found for the given input" });
+	} else {
+		res.redirect(ShortURLs[index-1]);
+	}
 });
 
 app.listen(port, function() {
